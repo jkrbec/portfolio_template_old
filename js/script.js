539a5e3c95ee54865c9d200bdb11a1898d095794
jkrbec <84323393+jkrbec@users.noreply.github.com>
2023-04-10 11:49:44 +0200

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-/* typing animation */
-var typed = new Typed(".typing", {
-    strings:["", "IT Enthusiast", "Teacher", "Father of Two", "Lifelong Learner"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
