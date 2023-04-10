de76335854cca52f41c494cb2147a129c9777d1c
jkrbec <84323393+jkrbec@users.noreply.github.com>
2023-04-10 11:42:52 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* typing animation */
 var typed = new Typed(".typing", {
-    strings:["", "IT Enthusiast", "Trainer", "Father of Two", "Lifelong Learner"],
+    strings:["", "IT Enthusiast", "Teacher", "Father of Two", "Lifelong Learner"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
